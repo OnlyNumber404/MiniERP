52cd41d07ba08bad40d7b5069c9b9869db90fc5b
OnlyNumber404 <agustinusdenny110@gmail.com>
2026-03-08 16:53:28 +0700

    fix auth login

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard',fn() => view('dashboard'));
 Route::get('/login',fn() => view('auth/login'))->name('login');
 Route::post('/login', [AuthControler::class,'login']);
